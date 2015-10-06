ab6b552477ad18b0fed7b98bb049ec6270ae28d6
Retzudo <chris@celeus.org>
2015-10-06 11:51:42 +0200

    gfycat capabilities

--- static/main.js
+++ static/main.js
@@ -1,6 +1,13 @@
 (function() {
     var app = angular.module('GifsApp', []);
 
+    app.config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'http://zippy.gfycat.com/**'
+        ]);
+    }]);
+
     app.controller('GifsController', ['$scope', '$http', function($scope, $http) {
         $scope.gifRows = [];
         $scope.search = '';
@@ -9,14 +16,20 @@
 
         $http.get('/gifs')
             .then(function(data) {
-                $scope.gifRows = _.chunk(data.data.gifs, 4);
-                allGifs = data.data.gifs;
+                allGifs = _.map(data.data.gifs, function (gif) {
+                    if (gif.gfycat && gif.gfycat.length > 0) {
+                        gif.webm = getWebmUrl(gif.gfycat);
+                        gif.mp4 = getMp4Url(gif.gfycat);
+                    }
+                    return gif;
+                });
+                $scope.gifRows = _.chunk(allGifs, 4);
             });
 
         $scope.filter = function() {
             var regex = new RegExp($scope.search, 'gi');
 
-            if ($scope.search.length == 0) {
+            if ($scope.search.length === 0) {
                 $scope.gifRows = _.chunk(allGifs, 4);
                 return;
             }
@@ -24,6 +37,14 @@
             $scope.gifRows = _.chunk(_.filter(allGifs, function(gif) {
                 return regex.test(gif.title) || regex.test(gif.tags.join(' '));
             }), 4);
+        };
+
+        function getWebmUrl(gfycat) {
+            return 'http://zippy.gfycat.com/' + gfycat + '.webm';
+        }
+
+        function getMp4Url(gfycat) {
+            return 'http://zippy.gfycat.com/' + gfycat + '.mp4';
         }
     }]);
 })();
