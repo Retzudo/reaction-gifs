74809435422403f6311c80604ac1810b0d9add73
Retzudo <christoph.willinger@gmail.com>
2015-12-06 15:40:12 +0100

    Material design

--- static/main.js
+++ static/main.js
@@ -1,29 +1,14 @@
 (function() {
+    "use strict";
+    
     var app = angular.module('GifsApp', []);
 
     app.controller('GifsController', ['$scope', '$http', function($scope, $http) {
-        $scope.gifRows = [];
+        $scope.gifs = [];
         $scope.search = '';
 
-        var allGifs = [];
-
-        $http.get('/gifs')
-            .then(function (response) {
-                $scope.gifRows = _.chunk(response.data.gifs, 4);
-                allGifs = response.data.gifs;
-            });
-
-        $scope.filter = function () {
-            var regex = new RegExp($scope.search, 'gi');
-
-            if ($scope.search.length === 0) {
-                $scope.gifRows = _.chunk(allGifs, 4);
-                return;
-            }
-
-            $scope.gifRows = _.chunk(_.filter(allGifs, function(gif) {
-                return regex.test(gif.title) || regex.test(gif.tags.join(' '));
-            }), 4);
-        };
+        $http.get('/gifs').then(function (response) {
+            $scope.gifs = response.data.gifs;
+        });
     }]);
 })();
