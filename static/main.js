5db8472b9163b2ce5b3f6f19a757a9d11d8f70f6
Retzudo <christoph.willinger@gmail.com>
2016-09-14 16:07:16 +0200

    added gif preview angular module

--- static/main.js
+++ static/main.js
@@ -1,14 +1,19 @@
 (function() {
     "use strict";
 
-    var app = angular.module('GifsApp', []);
+    var app = angular.module('GifsApp', ['gifPreview']);
 
     app.controller('GifsController', ['$scope', '$http', function($scope, $http) {
         $scope.gifs = [];
         $scope.search = '';
 
         $http.get('/gifs.json').then(function (response) {
-            $scope.gifs = response.data.gifs;
+            //$scope.gifs = response.data.gifs;
+            $scope.gifs = response.data.gifs.map(function (gif) {
+                gif.preview = '/preview/' + gif.url;
+                gif.cached = '/gif/' + gif.url;
+                return gif;
+            });
         });
     }]);
 }());
