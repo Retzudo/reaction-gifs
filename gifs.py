7e7ed1ec594371693a6c45b2586d3890b85e8606
Retzudo <christoph.willinger@gmail.com>
2016-09-14 12:21:59 +0200

    First steps towards GIF previews

--- gifs.py
+++ gifs.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, abort, jsonify, send_file
+from flask.ext.redis import FlaskRedis
 import yaml
 
 app = Flask(__name__)
+redis_store = FlaskRedis(app)
 
 
 @app.route('/')
@@ -24,6 +26,23 @@ def gifs():
 def favicon():
     return send_file('static/favicon.ico')
 
+
+@app.route('/preview/<url>')
+def preview(url):
+    """Get the key `url` from redis and serve that data with
+    MIME type 'image/gif'."""
+
+
+def cache_previews():
+    """Store preview images for GIFs listed in gifs.yml in redis.
+
+    1. download the image to a temporary location
+    2. extract the first frame of the gif using extract_frames.py
+    3. store that data in redis under the GIF's URL as the key
+    """
+
+# Run immediately on startup
+cache_previews()
 
 if __name__ == '__main__':
     app.run(debug=True)
