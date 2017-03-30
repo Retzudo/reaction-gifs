24319fc2f0fc1d0cb51c2ab12216fbc04df1c1e2
Retzudo <christoph.willinger@gmail.com>
2017-03-30 11:34:33 +0200

    Lazy image loading

--- static/main.js
+++ static/main.js
@@ -1,14 +1,18 @@
 (function() {
     "use strict";
 
-    var app = angular.module('GifsApp', []);
+    var app = angular.module('GifsApp', ['angularLazyImg']);
 
-    app.controller('GifsController', ['$scope', '$http', function($scope, $http) {
+    app.controller('GifsController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
         $scope.gifs = [];
         $scope.search = '';
 
         $http.get('/gifs.json').then(function (response) {
             $scope.gifs = response.data.gifs;
         });
+
+        $scope.lazyLoad = function () {
+            $rootScope.$emit('lazyImg:refresh');
+        }
     }]);
 }());
